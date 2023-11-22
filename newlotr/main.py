496be2d9c3e9dce9426e22d331c82fff8a94bdf1
Kaell Fernandes <silvakaell@gmail.com>
2023-11-22 11:32:10 -0300

    v.0.3

--- newlotr/main.py
+++ newlotr/main.py
@@ -5,13 +5,12 @@ API_KEY = "EpJZYL8_QwFZ05RCFL_j"
 
 headers = {
     "Authorization": f"Bearer {API_KEY}"
-
 }
 
 name = "Bilbo Baggins"
 id = ""
 
-# Chamo a API para retornar o ID a partir do nome do personagem
+# Chamo a API para retornar o ID a partir do nome do personagem #
 response = requests.get(f"https://the-one-api.dev/v2/character/", headers=headers)
 characters = response.json()["docs"]
 
@@ -19,12 +18,12 @@ for char in characters:
     if char["name"] == name:
         id = char["_id"]
 
-# Com o ID, chamo a api para retornar os quotes
+# Com o ID, chamo a api para colocar os quotes em uma lista #
 quote_response = requests.get(f"https://the-one-api.dev/v2/character/{id}/quote", headers=headers)
 quotes_list = quote_response.json()["docs"]
 quotes_select_list = []
 for quotes in quotes_list:
     quotes_select_list.append(quotes["dialog"])
 
-# Seleciona aleatoriamente um quote do personagem.
+# Seleciona aleatoriamente um quote do personagem #
 print(f"{name}: {random.choice(quotes_select_list)}")
